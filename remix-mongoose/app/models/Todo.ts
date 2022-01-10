5c25b6b2a145723a3970aa9976110f405b3d7a1f
Chandan Kumar <chandankumar.r@gmail.com>
2022-01-10 16:05:07 +0530

    folder move

--- remix-mongoose/app/models/Todo.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const TodoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-});
-const model = mongoose.models.Todo || mongoose.model("Todo", TodoSchema);
-export default model;
